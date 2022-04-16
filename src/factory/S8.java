b51d6e3e26a93f134115f8480cc66e3862480de7
oguzhan2027 <oguzhancelik2027@gmail.com>
2022-04-17 00:47:50 +0300

    factory design pattern

---
+++ src/factory/S8.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package factory;
+
+/**
+ *
+ * @author Oguzhan
+ */
+public class S8 implements Telefon {
+    private String model;
+    private String batarya;
+    private int en;
+    private int boy;
+
+    public S8(String model, String batarya, int en, int boy) {
+        this.model = model;
+        this.batarya = batarya;
+        this.en = en;
+        this.boy = boy;
+    }
+    
+    
+
+    @Override
+    public String getModel() {
+        return model;
+    }
+
+    @Override
+    public String getBatarya() {
+        return batarya;
+    }
+
+    @Override
+    public int getEn() {
+        return en;
+    }
+
+    @Override
+    public int getBoy() {
+        return boy;
+    }
+
+    @Override
+    public String toString() {
+        return "S8{" + "model=" + model + ", batarya=" + batarya + ", en=" + en + ", boy=" + boy + '}';
+    }
+    
+    
+    
+    
+    
+}
---
+++ src/factory/Telefon.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package factory;
+
+/**
+ *
+ * @author Oguzhan
+ */
+public interface Telefon {
+    String getModel();
+    String getBatarya();
+    int getEn();
+    int getBoy();
+    
+}
