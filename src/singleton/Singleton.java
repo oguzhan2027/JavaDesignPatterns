cf2d91123deb6881877c408a8b036a4ff53be5df
oguzhan2027 <oguzhancelik2027@gmail.com>
2022-04-17 00:22:38 +0300

    singleton design patterns

---
+++ src/singleton/Singleton.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package singleton;
+
+/**
+ *
+ * @author Oguzhan
+ */
+public class Singleton {
+
+    private static Singleton singleton;
+
+    private static int sayi = 0;
+
+    private Singleton() {
+        System.out.println("ben oluştum");
+    }
+
+    public static Singleton getSingleton() {
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+        sayi++;
+        System.out.println(sayi);
+        return singleton;
+    }
+
+}
