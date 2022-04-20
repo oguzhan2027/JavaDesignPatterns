59f5e0ca55663062e418217795e1cda1860dcd86
oguzhan2027 <oguzhancelik2027@gmail.com>
2022-04-20 11:28:05 +0300

    prototype design pattern

---
+++ src/prototype/BelgeTuru.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prototype;
+
+/**
+ *
+ * @author Oguzhan
+ */
+public class BelgeTuru implements Cloneable {
+    private Long id;
+    private String adi;
+
+    public BelgeTuru() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAdi() {
+        return adi;
+    }
+
+    public void setAdi(String adi) {
+        this.adi = adi;
+    }
+
+    @Override
+    public String toString() {
+        return adi;
+    }
+
+    @Override
+    protected BelgeTuru clone() throws CloneNotSupportedException {
+        return (BelgeTuru) super.clone(); 
+    }
+    
+    
+    
+}
