f7de5afae2f5577ab2b31d5a1b67485f1a04c37d
oguzhan2027 <oguzhancelik2027@gmail.com>
2022-04-20 11:29:11 +0300

    prototype design pattern

---
+++ src/prototype/Kategori.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prototype;
+
+/**
+ *
+ * @author Oguzhan
+ */
+public class Kategori implements Cloneable {
+    private Long id;
+    private String adi;
+
+    public Kategori() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAdi() {
+        return adi;
+    }
+
+    public void setAdi(String adi) {
+        this.adi = adi;
+    }
+
+    @Override
+    public String toString() {
+        return adi;
+    }
+
+    @Override
+    protected Kategori clone() throws CloneNotSupportedException {
+        return (Kategori) super.clone(); 
+    }
+    
+    
+}
