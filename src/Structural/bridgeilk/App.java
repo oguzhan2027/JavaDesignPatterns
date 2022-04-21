c5ca894f6b44e333cfba681e453c6a80d639a89f
oguzhan2027 <oguzhancelik2027@gmail.com>
2022-04-22 00:32:24 +0300

    bridge design pattern

---
+++ src/Structural/bridgeilk/App.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Structural.bridgeilk;
+
+/**
+ *
+ * @author Oguzhan
+ */
+public class App {
+    public static void main(String[] args) {
+        Muzik muzik = new Muzik("Sezen Aksu : Gülümse ","Gülümse hadi gülümse");
+        Bilgisayar bilgisayar = new Bilgisayar();
+        bilgisayar.muzikCal(muzik);
+    }
+    
+}
